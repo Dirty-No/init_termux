b35180eb387d4060ad6966b62859a4c8e00f7655
Noëlle <54525684+Dirty-No@users.noreply.github.com>
2020-10-12 16:03:38 +0200

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+pkg update && pkg upgrade
+
+pkg install vim bash zsh git wget curl openrc netcat
+echo 'alias vi="vim"' >> ~/.zshrc 
+printf "syntax on\nset nu\nset mouse=a" >> ~/.vimrc
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sed -i 's/robbyrussell/pygmalion/g' ~/.zshrc
+
+openrc boot
+
+yes 'root' | passwd
+
+export SERVICE_PORTS="8022"
+
+ifconfig | grep inet && echo "PORTS : $SERVICE_PORTS" && zsh
